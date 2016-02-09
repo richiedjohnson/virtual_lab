c3207ad12618d415743b9378431a9fa1887f22a0
Richie Johnson <richie@Richies-MacBook-Pro.local>
2016-02-09 02:46:23 -0500

    refixing 404 issue & 500

--- vital_site/vital/views/security_view.py
+++ vital_site/vital/views/security_view.py
@@ -17,13 +17,6 @@ from random import randint
 logger = logging.getLogger(__name__)
 
 
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
 def register(request):
     logger.debug("in register")
     error_message = ''
