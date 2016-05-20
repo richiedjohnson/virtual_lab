81b762709d2e6dc8b6974269cdc4a816f84b42da
Richie Johnson <richie@Richies-MBP.home>
2016-05-20 10:53:10 -0400

    environment changed to ini

--- vital_site/vital_site/wsgi.py
+++ vital_site/vital_site/wsgi.py
@@ -7,13 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os, sys
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
-sys.path.append(PROJECT_DIR)
+import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vital_site.settings")
 
-from django.core.wsgi import get_wsgi_application
-_application = get_wsgi_application()
+application = get_wsgi_application()
