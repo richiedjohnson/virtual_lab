1503e88b1e7d5cedf83f3400b47758493fdfc213
Richie Johnson <richie@Richies-MBP.home>
2016-05-20 09:47:49 -0400

    env update 4

--- vital_site/vital_site/settings.py
+++ vital_site/vital_site/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'vital_site.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 print "??????????????????????????"
-print "<>", os.environ.get("VITAL_DB_NAME", ""), "<>"
+print "<>", os.environ.get('VITAL_DB_NAME', ""), "<>"
 
 DATABASES = {
     'default': {
